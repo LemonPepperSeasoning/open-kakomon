2726ce045d3c5141b54fefcaf12838db06e2a23d
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-01-22 23:59:33 +0900

    Added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { useState } from "react";
+import Header from './components/Header/Header'
 import HomePage from './pages/HomePage';
+
 
 // import { useHistory } from 'react-router-dom';
 // import { useForm } from 'react-hook-form';
@@ -10,6 +12,7 @@ function App() {
   return (
     <>
       <div>
+        <Header/>
         <HomePage/>
       </div>
     </>
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+
+const TextField = () => {
+    return (
+    <>
+        <div className="flex bg-green-400">
+            
+
+            <h3 className="p-8">
+                CompanyLogo
+            </h3>
+
+            
+            <button>
+                Home
+            </button>
+        </div>
+        
+    </>
+    )
+}
+
+export default TextField;
