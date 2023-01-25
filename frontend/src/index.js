a40f519f4c5e866315b7c2d770f2b658a775e6b3
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-01-26 00:09:20 +0900

    Refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
 
       <Routes>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
 import { FaSearch } from 'react-icons/fa';
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer';
+
 
 function HomePage() {
-  
+
   // const history = useHistory();
   // const { register, handleSubmit } = useForm();
   // const [university, setUniversity] = useState('');
@@ -14,40 +18,38 @@ function HomePage() {
 
   return (
     <>
-      <div>
-        <div className="bg-gray-100">
-          <form className="max-w-lg p-10 mx-auto" onSubmit={onSubmit}>
-            <h1 className="p-5 text-2xl font-medium">Search for University Exam Papers</h1>
+      <div class="flex flex-col h-screen justify-between">
+        <header><Header /></header>
+        <main class="mb-auto">
+          <div>
+            <div className="bg-gray-100">
+              <form className="max-w-lg p-10 mx-auto" onSubmit={onSubmit}>
+                <h1 className="p-5 text-2xl font-medium">Search for University Exam Papers</h1>
 
-            <div className="flex w-full rounded-md">
+                <div className="flex w-full rounded-md">
 
-              <div className="justify-center w-full" >
-                <input name="university"
-                  className="w-full py-3 pl-5 rounded-md form-input"
-                  placeholder="University Name"
-                // ref={register({ required: true })}
-                />
-              </div>
+                  <div className="justify-center w-full" >
+                    <input name="university"
+                      className="w-full py-3 pl-5 rounded-md form-input"
+                      placeholder="University Name"
+                    // ref={register({ required: true })}
+                    />
+                  </div>
 
-              <button className="px-4">
-                <FaSearch className="w-5 text-blue-500" />
-              </button>
+                  <button className="px-4">
+                    <FaSearch className="w-5 text-blue-500" />
+                  </button>
+
+                </div>
+              </form>
 
             </div>
-          </form>
-
-        </div>
-
-
-        {/* <div className="p-10">
-          <h5>
-            Other content
-          </h5>
-        </div> */}
+          </div>
+        </main>
+        <footer> <Footer /></footer>
       </div>
     </>
   );
 }
 
 export default HomePage;
-  
