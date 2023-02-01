ecd42ee8455168ae3ec55907ee75eb13ae4c03e0
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-02-01 23:59:34 +0900

    Added university page

---
+++ frontend/src/pages/UniversityPage.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+function UniversityPage() {
+
+  return (
+    <>
+    <div>
+        <div>
+            <p>
+                University Name: {}
+            </p>
+            <p>
+                Address: {}
+            </p>
+            <p>
+                International Ranking: {}
+            </p>
+            <p>
+                National ranking: {}
+            </p>
+        </div>
+        
+
+        <div>
+            
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default UniversityPage;
