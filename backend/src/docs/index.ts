3a42e674dc6fe0ca19ed7a121a768da952ea5ecf
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-02-03 23:59:22 +0900

    Added auto gen

--- backend/src/docs/index.ts
+++ backend/src/docs/index.ts
@@ -3,5 +3,4 @@ import servers from "./servers";
 import components from "./components";
 import endpoints from "./API/index"
 
-
 export default { ...basicInfo, ...servers, ...components, ...endpoints}
