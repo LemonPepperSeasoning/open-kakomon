9f9c729cb0681f0c2cbaefd8b8aa13131a8f296b
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-02-08 00:34:29 +0900

    Added national & world ranking

--- frontend/src/components/SchoolPopularityList/SchoolPopularityList.js
+++ frontend/src/components/SchoolPopularityList/SchoolPopularityList.js
@@ -5,22 +5,37 @@ const SchoolPopularityList = () => {
     {
       "name": "University of Tokyo",
       "view_count": 1831,
+      "national_ranking": 1,
+      "country_location": "Japan",
+      "international_ranking": 30,
     },
     {
       "name": "University of Auckland",
       "view_count": 220,
+      "national_ranking": 1,
+      "country_location": "New Zealand",
+      "international_ranking": 30,
     },
     {
       "name": "University of Califonia",
       "view_count": 320,
+      "national_ranking": 1,
+      "country_location": "United States",
+      "international_ranking": 30,
     },
     {
       "name": "Stanford University",
       "view_count": 20,
+      "national_ranking": 1,
+      "country_location": "United States",
+      "international_ranking": 30,
     },
     {
       "name": "Massachusetts Institute of Technology",
       "view_count": 12,
+      "national_ranking": 1,
+      "country_location": "United States",
+      "international_ranking": 30,
     },
   ]
 
--- frontend/src/components/SchoolPopularityList/SchoolPopularityListItem.js
+++ frontend/src/components/SchoolPopularityList/SchoolPopularityListItem.js
@@ -1,10 +1,13 @@
 import { useState, useEffect, useCallback } from "react";
+import { countryList } from '../../utils/CountryNameToISO'
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const SchoolPopularityListItem = (props) => {
 
     console.log(props)
 
     const [imageSrc, setImageSrc] = useState("default_school_logo.jpg");
+    const countryCode = countryList[props.school.country_location].toLowerCase();
 
     // declare the async data fetching function
     const fetchImage = useCallback(async () => {
@@ -13,12 +16,15 @@ const SchoolPopularityListItem = (props) => {
         setImageSrc(data);
     }, [])
 
-    // the useEffect is only there to call `fetchData` at the right time
-    useEffect(() => {
-        fetchImage()
-            .catch(console.error);;
-    }, [fetchImage])
+    const setCountry =
 
+        // the useEffect is only there to call `fetchData` at the right time
+        useEffect(() => {
+            fetchImage()
+                .catch(console.error);;
+        }, [fetchImage])
+
+    // UNICODE FROM : https://unicode-table.com/en/search/?q=world
     return (
         <>
             <div className="flex p-1 m-3 m-auto rounded-md w-80 bg-slate-200">
@@ -29,11 +35,22 @@ const SchoolPopularityListItem = (props) => {
 
                 <div className="p-1 w-60">
                     <div className="overflow-hidden font-bold text-ellipsis whitespace-nowrap">
+
                         {props.school.name}
                     </div>
 
-                    <div>
-                        {props.school.view_count}
+
+                    <div className="flex">
+
+                        <div className="mr-4">
+                            🌎#{props.school.international_ranking}
+                        </div>
+
+                        <div className="mr-4">
+                            <span class={"fi fi-" + countryCode}></span>#{props.school.national_ranking}
+                        </div>
+
+                        {props.school.view_count} views
                     </div>
                 </div>
             </div>
