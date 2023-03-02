151937ad62233dd34da75136bf2c215c3428d247
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-03-02 10:40:36 -0500

    clearned upload exam page

--- frontend/src/pages/PostExamPage.js
+++ frontend/src/pages/PostExamPage.js
@@ -11,27 +11,99 @@ const PostExamPage = () => {
 
     return (
         <>
-            <div className="m-auto">
-                <form>
-                    <form onSubmit={onSubmit}>
+            <div className="w-2/3 max-w-screen-sm m-auto">
 
-                        <label for="fname">{t("school_name")}:</label><br />
-                        <input className="m-auto border-2 border-black rounded-md" type="text" id="fname" name="fname" /><br /><br />
+                <form action="#" method="POST">
 
-                        <label for="lname">{t("subject")}:</label><br />
-                        <input className="border-2 border-black rounded-md" type="text" id="lname" name="lname" /><br /><br />
+                    <div class="mb-6">
+                        <label for="school-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            {t("school_name")}
+                        </label>
+                        <input
+                            type="school-name"
+                            id="school-name"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="University of Tokyo"
+                            required />
+                    </div>
 
-                        <label for="lname">{t("year_of_exam")}:</label><br />
-                        <input className="border-2 border-black rounded-md" type="number" id="lname" name="lname" /><br /><br />
+                    <div class="mb-6">
+                        <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            {t("subject")}
+                        </label>
+                        <input
+                            type="subject"
+                            id="subject"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Eg, Calculus, History, Geography"
+                            required />
+                    </div>
 
-                        <label for="lname">{t("file")}:</label><br />
-                        <input className="border-2 border-black rounded-md" type="file" id="files" name="lname" multiple /><br /><br />
+                    <div class="mb-6">
+                        <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            {t("year_of_exam")}
+                        </label>
+                        <input
+                            type="year"
+                            id="year"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="2022"
+                            required />
+                    </div>
 
 
-                        <input className="border-2 border-black rounded-md" type="submit" value={t("submit")} />
-                    </form>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">{t("file")}</label>
+                        <div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
+                            <div className="space-y-1 text-center">
+                                <svg
+                                    className="w-12 h-12 mx-auto text-gray-400"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 48 48"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                                <div className="flex text-sm text-gray-600">
+                                    <label
+                                        htmlFor="file-upload"
+                                        className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                                    >
+                                        <span>Upload a file</span>
+                                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                    </label>
+                                    <p className="pl-1">or drag and drop</p>
+                                </div>
+                                <p className="text-xs text-gray-500">PNG, JPG, PDF up to 10MB</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex items-start mb-6 mt-3">
+                        <div class="flex items-center h-5">
+                            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                        </div>
+                        <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
+                    </div>
+
+
+                    <div className="px-4 py-3 text-right sm:px-6">
+                        <button
+                            type="submit"
+                            className="inline-flex justify-center px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                        >
+                            Save
+                        </button>
+                    </div>
                 </form>
             </div>
+
         </>
     )
 }
