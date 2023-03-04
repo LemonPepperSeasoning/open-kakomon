f8ada69e24a4dca23c445100088ba21d3ccdda33
LemonPepperSeasoning <takahiro.swe@gmail.com>
2023-03-04 21:15:16 +0530

    removed textarea outline

--- frontend/src/components/ReviewLists/ReviewLists.js
+++ frontend/src/components/ReviewLists/ReviewLists.js
@@ -43,11 +43,9 @@ const ReviewLists = () => {
         }
     }, [])
 
-
     const submitReview = (e) => {
         e.preventDefault();
-        console.log(name);
-        console.log(textarea);
+        console.log(e);
         const newReview = {
             comment: textarea,
             username: name
@@ -91,11 +89,16 @@ const ReviewLists = () => {
             </div> */}
 
 
-            <form>
+            <form onSubmit={submitReview}>
                 <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                    <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                    <div class="px-4 py-4 bg-white rounded-t-lg dark:bg-gray-800">
                         <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
+                        <textarea
+                            id="comment"
+                            rows="4"
+                            class="border-0 outline-none w-full px-0 text-sm text-gray-900 bg-white dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                            placeholder="Write a comment..."
+                            required />
                     </div>
                     <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
                         <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
@@ -110,6 +113,9 @@ const ReviewLists = () => {
                     </div>
                 </div>
             </form>
+
+
+
 
             <div className="rounded-md">
                 {dummayReview.map((object, id) => (<ReivewListsItem review={object} />))}
